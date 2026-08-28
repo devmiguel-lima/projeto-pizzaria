4387e39200f1328abedf2ccb48901cb5d0dd28d8
Dev-Miguellima <dev.miguellima@gmail.com>
2026-08-28 14:36:15 -0300

    feat: Implementar a funcionalidade de criação de usuário com controller e service.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,11 @@
 import { Request, Response, Router } from "express";
 
+import { CreateUserController} from './controllers/user/CreateUserController'
+
+
 const router = Router();
 
-router.post("/users", (req: Request, res: Response) => {
-  res.json({ message: "FUNCIONANDO!!!!" });
-});
+router.post("/users", new CreateUserController().handle);
 
 export {router}
 
---
+++ backend/src/services/user/CreateUserService.ts
@@ -0,0 +1,9 @@
+class CreateUserService {
+  async execute() {
+    console.log("SERVICO EXECUTADO");
+
+    return "USUARIO MIGUEL CRIADO!";
+  }
+}
+
+export { CreateUserService };
