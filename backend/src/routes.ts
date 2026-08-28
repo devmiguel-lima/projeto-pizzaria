e5550a1a36dd6c08a56ca28d5550640a08ec013b
Dev-Miguellima <dev.miguellima@gmail.com>
2026-08-28 15:36:42 -0300

    feat: Adicionar schema de validação de usuário e middleware para a criação de usuário.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,15 +1,21 @@
 import { Request, Response, Router } from "express";
 
-import { CreateUserController} from './controllers/user/CreateUserController'
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
+import { validateSchema } from "./middlewares/validateSchema";
+
+import { createUserSchema } from "./schemas/userSchema";
 
 const router = Router();
 
-router.post("/users", new CreateUserController().handle);
+router.post(
+  "/users",
+  validateSchema(createUserSchema),
+  new CreateUserController().handle,
+);
 
-export {router}
+export { router };
 
 //ARQUITETURA EM CAMADAS ROUTES-CONTROLLER-SERVICE
-//CONTROLLER > 
+//CONTROLLER >
 //SERVICE > RESPONSAVEL PELA LOGICA POR REALAIZAR OPERAÇOES
-
